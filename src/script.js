c8a2ad09c48893113ac8acbc016f84f8a76043cb
Geshina <geshovskileo@gmail.com>
2024-02-03 18:11:38 +0100

    state finnished

--- src/script.js
+++ src/script.js
@@ -6,15 +6,84 @@
 //  Enter = update tasks with textarea content
 //  Focus = sync data across tabs
 
-// make new key dumbo
-let key = "rhugtkeldibnridrlerlgcrrdvneevit";
-
 // VARIABLES
 
+let key = "mmjauwoofoink";
 let textarea = document.querySelector("textarea");
 let tasks = document.querySelector(".tasks");
-// FUNCTIONS
 
+//  STATIC
+
+// chrome_set(key, "");
+datePercent();
+progressBar(datePercent);
+
+(async () => {
+  tasks.innerHTML = await chrome_get(key);
+  click_listener();
+})();
+
+//  ENTER
+
+textarea.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+
+    // transfer to textarea + append
+    let para = document.createElement("p");
+    para.innerHTML = textarea.value;
+    para.classList.add("task");
+    // para.setAttribute("tabIndex", "0");
+    tasks.append(para);
+    textarea.value = "";
+
+    click_listener();
+    chrome_set(key, tasks.innerHTML);
+  }
+});
+
+// chrome.storage.onChanged
+//  FOCUS
+
+chrome.tabs.onActivated.addListener(async () => {
+  tasks.innerHTML = await chrome_get(key);
+  click_listener();
+});
+
+chrome.windows.onFocusChanged.addListener(async () => {
+  tasks.innerHTML = await chrome_get(key);
+  click_listener();
+});
+
+//  Functions
+
+// state managment
+function click_listener() {
+  let task_all = document.querySelectorAll(".task");
+
+  // Duplicates negate all functionality
+  task_all.forEach((task) => {
+    task.removeEventListener("click", handleClick);
+    task.addEventListener("click", handleClick);
+
+    function handleClick() {
+      task.classList.toggle("task-clicked");
+      chrome_set(key, tasks.innerHTML);
+    }
+  });
+}
+
+// storage
+async function chrome_get(key) {
+  let result = await chrome.storage.sync.get(key);
+  return result[key];
+}
+function chrome_set(key, data) {
+  let temp_obj = { [key]: data };
+  chrome.storage.sync.set(temp_obj);
+}
+
+// date
 function datePercent() {
   let date_start = Date.parse("17 Apr 2004 00:00:00 GMT+1");
   let date_now = Date.now();
@@ -28,73 +97,19 @@ function progressBar(datePercent) {
     (100 - date_percent).toPrecision(5) + "%";
 }
 
-async function chrome_get(key) {
-  let result = await chrome.storage.sync.get(key);
-  return result[key];
-}
+// cut + undo
+let pressedKeys = {};
+document.addEventListener("keydown", (e) => {
+  pressedKeys[e.key] = true;
 
-function chrome_set(key, data) {
-  let temp_obj = { [key]: data };
-  chrome.storage.sync.set(temp_obj);
-}
+  if (pressedKeys["Control"] && pressedKeys["Shift"] && pressedKeys["X"]) {
+    let clicked = document.querySelectorAll(".task-clicked");
 
-//  STATIC
+    clicked.forEach((element) => {
+      element.remove();
+    });
 
-// chrome_set(key, "");
-datePercent();
-progressBar(datePercent);
-
-(async () => {
-  let temp = await chrome_get(key);
-  tasks.innerHTML = temp;
-  task_listeners();
-})();
-
-//  ENTER
-
-textarea.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    e.preventDefault();
-
-    // transfer to textarea + append
-    let para = document.createElement("p");
-    para.innerHTML = textarea.value;
-    para.classList.add("task");
-    tasks.append(para);
-    textarea.value = "";
-
-    task_listeners();
-    chrome_set(key, tasks.innerHTML);
+    chrome_set(key, textarea.innerHTML);
+    pressedKeys = {};
   }
-});
-
-// state managment
-// paramterize the shit out of this one
-function task_listeners() {
-  let task_all = document.querySelectorAll(".task");
-
-  task_all.forEach((task) => {
-    task.addEventListener("mouseover", (e) => {
-      task.classList.replace("task", "task-hover");
-    });
-    task.addEventListener("mouseout", (e) => {
-      task.classList.replace("task-hover", "task");
-    });
-    task.addEventListener("click", (e) => {
-      task.classList.toggle("task-clicked");
-      chrome_set(key, tasks.innerHTML);
-    });
-  });
-}
-
-//  FOCUS
-
-// not tab based only updates on alt + tab not ctrl + tab
-chrome.windows.onFocusChanged.addListener(() => {
-  (async () => {
-    let temp = await chrome_get(key);
-    tasks.innerHTML = temp;
-    task_listeners();
-  })();  
-
 });
